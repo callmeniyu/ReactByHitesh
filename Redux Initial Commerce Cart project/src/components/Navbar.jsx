a47b4a7cf7ffb5ce54e69175210e4ee8494f074d
callmeniyu <getniyashere@gmail.com>
2023-12-15 21:51:16 +0530

    navbar responsiveness set

--- Redux Initial Commerce Cart project/src/components/Navbar.jsx
+++ Redux Initial Commerce Cart project/src/components/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react"
+import { NavLink } from "react-router-dom"
 
 function Navbar() {
-  return (
-      <div className='tw-flex'>
-          <h3>Redux tool kit</h3>
-          <NavLink to="/">
-              Products
-          </NavLink>
-          <NavLink to="cart">
-              Add
-          </NavLink>
-    </div>
-  )
+    const [isClicked, setIsClicked] = useState(false)
+    return (
+        <>
+            <div className="tw-flex  tw-justify-between">
+                <h3>Redux tool kit</h3>
+                <div className=" tw-flex nav-items tw-gap-5">
+                    <NavLink to="/">Products</NavLink>
+                    <NavLink to="cart">Add</NavLink>
+                </div>
+                <h1 className="plus tw-cursor-pointer" onClick={()=>setIsClicked((prev)=>!prev)}>+</h1>
+            </div>
+
+            <div className={`nav-div tw-flex tw-flex-col   ${isClicked ? "show" : "hide"}`}>
+                <NavLink to="/">hi</NavLink>
+                <NavLink to="cart">there</NavLink>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
